0d50e16411df1f233981f8c2dc54bcaa6668c16a
ada lovecraft <jeremy@codevinsky.com>
2016-12-17 07:45:52 -0600

    Creating a proper master branch

---
+++ README.md
@@ -0,0 +1,29 @@
+# Asteroids
+
+[![Join the chat at https://gitter.im/CodingRainbow/Asteroids](https://badges.gitter.im/CodingRainbow/Asteroids.svg)](https://gitter.im/CodingRainbow/Asteroids?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+
+This repo is for the Asteroids project from Coding Challenge 45.
+
+### Video Tutorials
+1. Part 1: https://youtu.be/hacZU523FyM
+2. Part 2: https://youtu.be/xTTuih7P0c0
+3. Stay tuned for Siraj's tutorial!
+
+### Play the game!
+* https://codingrainbow.github.io/Asteroids/
+
+### Info
+
+The code here is an updated and improved version of the game based on viewer submissions via pull requests.  The original code from the challenge itself can be found here:
+
+https://github.com/CodingRainbow/Rainbow-Code/tree/master/challenges/CC_46_Asteroids
+
+To submit a new issue, [file an issue describing your idea](https://github.com/CodingRainbow/Asteroids/issues/new) and [make a pull request](https://github.com/CodingRainbow/Asteroids/pulls)! If you make your own version of the game in a separate repo or fork, pull request this README file to add a link.
+
+I'll make a video about how to contribute soon too!
+
+### Viewer Remixes of the Game
+
+* Add your name and link here!
+* Serge Korzh - https://github.com/qw8/Asteroids-1
+* Timo Köhler - https://navetk.github.io/Asteroids/
---
+++ laser.js
@@ -0,0 +1,54 @@
+// Daniel Shiffman
+// http://codingrainbow.com
+// http://patreon.com/codingrainbow
+// Code for: https://youtu.be/hacZU523FyM
+
+var colors = [[248, 12, 18], [238, 17, 0], [255, 51, 17], [255, 68, 34], [255, 102, 68], [255, 153, 51], [254, 174, 45], [204, 187, 51], [208, 195, 16], [170, 204, 34], [105, 208, 37], [34, 204, 170], [18, 189, 185], [17, 170, 187], [68, 68, 221], [51, 17, 187], [59, 12, 189], [68, 34, 153]]
+
+function Laser(spos, angle) {
+  Entity.call(this, spos.x, spos.y, 4);
+
+  this.pos = createVector(spos.x, spos.y);
+  this.vel = p5.Vector.fromAngle(angle);
+  this.vel.mult(10);
+  this.color = colors[floor(random(0,colors.length-1))];
+
+  this.render = function() {
+    push();
+    stroke(this.color[0], this.color[1], this.color[2]);
+    strokeWeight(this.r);
+    point(this.pos.x, this.pos.y);
+    pop();
+  }
+
+  this.playSoundEffect = function(sound){
+    if (!sound.isPlaying()){
+      sound.play();
+    }
+  }
+
+  this.hits = function(asteroid) {
+    var last_pos = p5.Vector.sub(this.pos, this.vel);
+    var asteroid_vertices = asteroid.vertices();
+    for(var i = 0; i < asteroid_vertices.length - 1; i++) {
+      if(lineIntersect(last_pos, this.pos, asteroid_vertices[i], asteroid_vertices[i + 1])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  this.offscreen = function() {
+    if (this.pos.x > width || this.pos.x < 0) {
+      return true;
+    }
+    if (this.pos.y > height || this.pos.y < 0) {
+      return true;
+    }
+    return false;
+  }
+
+
+}
+
+Laser.prototype = Object.create(Entity.prototype);
